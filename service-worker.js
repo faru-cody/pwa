db64d8c967345df6b215029626d0cab5295112f4
Faru Code <161667014+faru-cody@users.noreply.github.com>
2024-11-05 21:30:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('v1').then(function(cache) {
+        caches.open('my-cache').then(cache => {
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/styles.css',
                 '/script.js',
-                // Add more files as needed
+                '/icon-192x192.png',
+                '/icon-512x512.png'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
