38d988b5245f17296d41f73e2134191a6ba628c8
Faru Code <161667014+faru-cody@users.noreply.github.com>
2024-11-05 21:50:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,22 @@
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('my-cache').then(cache => {
+        caches.open('v1').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/styles.css',
                 '/script.js',
                 '/icon-192x192.png',
-                '/icon-512x512.png'
+                '/icon-512x512.png',
+                // Aur bhi files add karo
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
